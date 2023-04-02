ff6a293390656b5f8252d7b38d62fd62dbe03bf1
fredkorts <fredkorts@gmail.com>
2023-04-02 17:18:13 +0300

    Use the SwiperComponent component in the App file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
-import SelectWithRandomOptions from './components/Select/SelectWithRandomOptions'
+import SwiperComponent from './components/Select/Swiper/SwiperComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Let's create a random array of numbers for our select component.
+  // const options = Array.from({ length: 30 }, () => Math.floor(Math.random() * 10000));
+
+  const handleSelectValue = (val) => {
+    setSelectedValue(val);
+  }
+
+  // Fetch some random numbers data from an API.
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [options, setOptions] = useState([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await fetch('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=30');
+        const data = await response.json();
+        setOptions(data);
+
+      } catch (error) {
+        console.log(`Failed to fetch the data: ${error}`);
+      }
+    }
+    fetchData();
+  },[])
 
   return (
     <div className="App">
       <div className="card">
-        <SelectWithRandomOptions />
+        <SwiperComponent
+            options={options} 
+            value={selectedValue}
+            backgroundColor={"#fff"}
+            primaryColor={"#474747"} 
+            secondaryColor={"#00817a"} 
+            subtitle={"€/Month"} 
+            keyboardEnabled
+            onSelectValue={handleSelectValue}
+          />
       </div>
     </div>
   )
